b3fa70f332cde4a19820c8b55d60de9f27ea9ed8
Changry <c@changry.no>
2021-08-24 13:46:48 +0200

    Added data class to server.py

--- Server/server.py
+++ Server/server.py
@@ -1,9 +1,22 @@
+from datetime import datetime
 from flask import Flask, request
 app = Flask(__name__)
+
+class Data:
+    def __init__(self, request) -> None:
+        self.request = request
+        def extract(key):
+            return self.request.form[key]
+        
+        self.code = extract("code")
+        self.timestamp = datetime.utcnow()
+        self.vote = extract("vote")
+        self.classname = extract("class")
+
+
 @app.route('/vote', methods=['POST'])
 def result():
-    print(request.form['fname']) # should display 'bar'
-    return 'Received !' # response to your request.
+    data = Data(request)
 
 
 app.run(host="127.0.0.1", port=8080, debug=False)
