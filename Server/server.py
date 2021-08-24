b4b13966b515942a6336054bdfa8f5467165b9eb
IceWafflez <knutstrandskogen@gmail.com>
2021-08-24 14:31:19 +0200

    siste push tirsdag

--- Server/server.py
+++ Server/server.py
@@ -1,22 +1,10 @@
-from datetime import datetime
 from flask import Flask, request
 app = Flask(__name__)
-
-class Data:
-    def __init__(self, request) -> None:
-        self.request = request
-        def extract(key):
-            return self.request.form[key]
-        
-        self.code = extract("code")
-        self.timestamp = datetime.utcnow()
-        self.vote = extract("vote")
-        self.classname = extract("class")
-
-
 @app.route('/vote', methods=['POST'])
 def result():
-    data = Data(request)
+    print(request.form['fname']) # should display 'bar'
+    return 'Received !' # response to your request.
+    retunm
 
 
 app.run(host="127.0.0.1", port=8080, debug=False)
