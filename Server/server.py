7bc91b05448641146448067dae60ee73b2afc82c
Changry <c@changry.no>
2021-08-25 12:15:37 +0200

    Server now writes to DB

--- Server/server.py
+++ Server/server.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 import sqlite3
 from flask import Flask, request
-from flask_cors import CORS
 app = Flask(__name__)
 
 
-
-
+DB_PATH = "DB/valg.db"
 
 
 
@@ -22,24 +20,25 @@ class Data:
         self.ts = datetime.utcnow()
 
 
-@app.route('/vote', methods=['POST'])
-@cross_origin()
+@app.route("/vote", methods=["POST"])
 def result():
     data = Data(request)
-    conn = sqlite3.connect('DB/valg.db')
+    conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    
+
     c.execute(f"SELECT COUNT(*) FROM voter WHERE token='{data.token}' AND token_used=0 AND vote_class='{data.classname}'")
     is_token_available = bool(c.fetchone()[0])
-    
+
     if is_token_available == True:
         c.execute(f"UPDATE voter SET token_used = 1 WHERE token='{data.token}'")
         c.execute(f"INSERT INTO votes VALUE ({data.token}, {data.ts}, {data.vote})")
         conn.commit()
-    
+
     return str(is_token_available)
 
 
 #-----------------------------------------------------------------------------------------
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=False)
+
+
